ef1e5bd5033e7a61635fdbafc7964a386b5dc3d1
guiurm <guiurm@gmail.com>
2024-09-18 01:54:13 +0200

    cd!: remove cd

--- src/components/index/InfoCard.vue
+++ src/components/index/InfoCard.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
+import { computed } from 'vue'
+
 defineSlots<{ title: {}; default: {} }>()
-defineProps<{ reverse?: boolean; img: { src: string; alt: string } }>()
+const props = defineProps<{ reverse?: boolean; img: { src: string; alt: string } }>()
+
+const src = computed(() => `/online-test${props.img.src}`) //import.meta.env.DEV === true ? props.img.src : `/online-test${props.img.src}`))
 </script>
 <template>
     <!--
@@ -22,7 +26,7 @@ defineProps<{ reverse?: boolean; img: { src: string; alt: string } }>()
     <a
         class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-2xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
     >
-        <img class="object-contain w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg p-8 md:p-3" :src="img.src" :alt="img.alt" />
+        <img class="object-contain w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg p-8 md:p-3" :src="src" :alt="img.alt" />
         <div class="flex flex-col justify-between p-4 leading-normal">
             <h5 class="mb-2 text-2xl font-bold tracking-tight">
                 <slot name="title" />
