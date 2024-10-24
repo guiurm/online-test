35683c9e0c9c10aad71e3eab2d9363390ed7b55d
guiurm <guiurm@gmail.com>
2024-10-24 20:10:27 +0200

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue(), vueJsx()],
-    base: '', //'/online-test/',
+    //base: '', //'/online-test/',
     server: { port: 80 },
     resolve: {
         alias: {
